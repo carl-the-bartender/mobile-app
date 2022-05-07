42ca5750eb52dbb850333713be748a79fc9ec146
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-05-07 12:18:37 +0200

    Adding names to a list

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [personName, setPersonName] = useState('');
+  const [persons, setPersons] = useState([{
+    'name': 'John doe'
+  }, {
+    'name': 'Jane dora'
+  }]);
+
+  const handleOnPress = () => {
+    setPersons([...persons, { name: personName }]);
+    setPersonName('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={text => setPersonName(text)}
+          value={personName}
+          placeholder="useless placeholder"
+        />
+      </SafeAreaView>
+      <Button
+        onPress={handleOnPress}
+        title="Add person"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      {persons.map((item, index) => {
+        return <Text>{item.name}</Text>
+      })}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFFF00',
+    padding: 50
   },
+  input: {
+    backgroundColor: 'white',
+    width: '100%',
+  }
 });
